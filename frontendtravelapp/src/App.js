259124f8b2973fea1b720bb3182315d800b5bc8f
kavyasomagit <kavyasoma17@gmail.com>
2023-11-20 18:34:31 -0500

    week 3

--- frontendtravelapp/src/App.js
+++ frontendtravelapp/src/App.js
@@ -1,8 +1,14 @@
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
-import {Home} from './pages'
+import {Home, SingleHotel} from './pages'
+
 function App() {
   return (
-    <Home/>
+    <Routes>
+      <Route path="/" element = {<Home/>} />
+      <Route path="/hotels/:name/:address/:id/reserve" element = {<SingleHotel/>}/>
+    </Routes>
+    
   );
 }
 
